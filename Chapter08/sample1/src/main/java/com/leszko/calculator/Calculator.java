559a5fcb752b8b481507c09a7f583656d357319d
Karen Ann <karen_piotrowski@student.uml.edu>
2024-02-23 18:26:52 -0500

    Update Calculator.java to add javadoc comment on line 6

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,6 +3,10 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+* w5ex2
+* Insert Javadoc comment to satisfy checkstyle.
+*/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;

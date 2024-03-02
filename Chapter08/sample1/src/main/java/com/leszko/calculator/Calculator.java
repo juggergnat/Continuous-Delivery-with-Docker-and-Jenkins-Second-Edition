9ea8ad7636fc64d071042b9a9161834028342c49
Karen Ann <karen_piotrowski@student.uml.edu>
2024-03-02 12:35:15 -0500

    fix all

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,9 +3,13 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+/**
+* w5ex2
+* Insert Javadoc comment to satisfy checkstyle.
+*/
 @Service
 public class Calculator {
-        final static int umlNUMBER1 = 3;
+        final static int UML_NUMBER_1 = 3;
 	@Cacheable("sum")
 	public int sum(int a, int b) {
 		return a + b;
